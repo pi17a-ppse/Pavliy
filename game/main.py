1a318032e1d43c5df10c9b6e4af21b26226c94dc
alinkliy <alina1530p@gmail.com>
2021-03-09 23:43:15 +0300

    Added size display and added fonts

--- game/main.py
+++ game/main.py
@@ -7,3 +7,17 @@ yellow = (255, 255, 102)
 black = (0, 0, 0)
 red = (213, 50, 80)
 green = (0, 255, 0)
+
+dis_width = 600
+dis_height = 400
+
+dis = pygame.display.set_mode((dis_width, dis_height))
+pygame.display.set_caption('Snake Game')
+
+clock = pygame.time.Clock()
+
+snake_block = 10
+snake_speed = 15
+
+font_style = pygame.font.SysFont("meiryo", 25)
+score_font = pygame.font.SysFont("meiryo", 45)
