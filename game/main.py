6a5d56f94e83e488a675eb3cdb81726661fa1280
alinkliy <alina1530p@gmail.com>
2021-03-10 18:01:10 +0300

    Used events K_UP, K_DOWN, K_LEFT, K_RIGHT

--- game/main.py
+++ game/main.py
@@ -23,11 +23,39 @@ font_style = pygame.font.SysFont("meiryo", 25)
 score_font = pygame.font.SysFont("meiryo", 45)
 
 game_over = False
+
+x1 = 300
+y1 = 300
+
+x1_change = 0  # for change x
+y1_change = 0  # for change y
+
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x1_change = -10
+                y1_change = 0
+            elif event.key == pygame.K_RIGHT:
+                x1_change = 10
+                y1_change = 0
+            elif event.key == pygame.K_UP:
+                y1_change = -10
+                x1_change = 0
+            elif event.key == pygame.K_DOWN:
+                y1_change = 10
+                x1_change = 0
+
+    x1 += x1_change
+    y1 += y1_change
+    dis.fill(white)
+    pygame.draw.rect(dis, black, [x1, y1, 10, 10])
+
+    pygame.display.update()
+
+    clock.tick(30)
 
 pygame.quit()
 quit()
-
