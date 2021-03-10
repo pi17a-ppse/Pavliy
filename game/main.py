98123fe7e268b5d1f2b144afcca99be8360da100
alinkliy <alina1530p@gmail.com>
2021-03-10 17:39:55 +0300

    Added QUIT

--- game/main.py
+++ game/main.py
@@ -25,7 +25,8 @@ score_font = pygame.font.SysFont("meiryo", 45)
 game_over = False
 while not game_over:
     for event in pygame.event.get():
-        print(event)
+        if event.type == pygame.QUIT:
+            game_over = True
 
 pygame.quit()
 quit()
